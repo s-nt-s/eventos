6d0b3b4d72b9f36257da58c08100956089a527c0
s-nt-s <santos82h@gmail.com>
2025-11-30 16:53:04 +0100

    more

--- core/util/madriddestino.py
+++ core/util/madriddestino.py
@@ -1,14 +1,17 @@
 from core.web import WEB
-from core.util import get_a_href
+from core.util import get_a_href, get_domain
 from functools import cache
 
 KO_MORE = (
     None,
+    'imccwem.munimadrid.es'
 )
+
 
 @cache
 def find_more_url(url: str):
     soup = WEB.get_cached_soup(url)
     href = get_a_href(soup.select_one("a.c-mod-file-event__content-link"))
-    if href and href not in KO_MORE:
+    dom = get_domain(href)
+    if href and href not in KO_MORE and dom not in KO_MORE:
         return href
