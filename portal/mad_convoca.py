3435839086f9ad2f40f9e3952e0340fefcdda537
s-nt-s <santos82h@gmail.com>
2025-12-20 11:19:16 +0100

    mad_convoca

--- portal/mad_convoca.py
+++ portal/mad_convoca.py
@@ -26,7 +26,8 @@ class MadConvoca:
         def _mk_key_mame_place(e: Event):
             name = plain_text(e.name) or ''
             compact = re_sp.sub("", name)
-            for s in (compact, name):
+            only_w = re.sub(r"\w", "", compact)
+            for s in (only_w, compact, name):
                 if len(s) > 10:
                     return (e.place, s)
 
