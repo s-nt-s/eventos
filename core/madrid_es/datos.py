8848246aa6c22638f67debdbb7b95c43748515d3
s-nt-s <santos82h@gmail.com>
2026-01-19 21:19:37 +0100

    madrides

---
+++ core/madrid_es/datos.py
@@ -0,0 +1,344 @@
+import logging
+from typing import NamedTuple, Optional
+from core.dictwraper import DictWrapper
+from datetime import datetime, date
+from json.decoder import JSONDecodeError
+from core.filemanager import FileManager
+from core.cache import TupleCache
+from core.util import get_obj
+import json
+import re
+from aiohttp import ClientResponse
+from core.fetcher import Getter
+from bs4 import BeautifulSoup
+
+
+logger = logging.getLogger(__name__)
+re_sp = re.compile(r"\s+")
+re_org_api = re.compile(r"^https://datos\.madrid\.es/egob/catalogo/tipo/entidadesyorganismos/\S+\.json$")
+
+
+async def rq_to_json(r: ClientResponse):
+    try:
+        return await r.json()
+    except JSONDecodeError:
+        text = await r.text()
+        text = re_sp.sub(r" ", text).strip()
+        if len(text) == 0:
+            logger.critical(f"{r.url} is empty: {text}")
+        try:
+            return json.loads(text)
+        except Exception:
+            pass
+        logger.critical(f"{r.url} is not a JSON: {text}")
+        raise
+
+
+async def rq_to_graph_list(r: ClientResponse) -> list[dict]:
+    obj = await rq_to_json(r)
+    if not isinstance(obj, dict):
+        raise ValueError(f"data is not a dict {r.url}")
+    lst = obj.get("@graph")
+    if not isinstance(lst, list):
+        raise ValueError(f"@graph is not list {r.url}")
+    if len(lst) == 0:
+        logger.critical(f"@graph is empty list {r.url}")
+        return []
+    if not all(isinstance(x, dict) for x in lst):
+        raise ValueError(f"@graph is not list[dict] {r.url}")
+    return list(FileManager.parse_obj(i, compact=True) for i in lst)
+
+
+async def rq_to_graph_list_len_1(r: ClientResponse):
+    graph = await rq_to_graph_list(r)
+    if len(graph) != 1:
+        raise ValueError(f"@graph is not len(list[dict]) == 1 {r.url}")
+    obj = FileManager.parse_obj(graph[0], compact=True) or {}
+    return obj
+
+
+async def rq_to_label(r: ClientResponse):
+    soup = BeautifulSoup(await r.read(), "xml")
+    node = soup.find("skos:prefLabel", attrs={"xml:lang": "es"})
+    if node is None:
+        node = soup.find("skos:prefLabel")
+    if node is not None:
+        txt = re_sp.sub(" ", node.get_text()).strip()
+        return txt
+    node = soup.find("rdf:Description")
+    about = node.attrs.get('rdf:about') if node else None
+    if about is None:
+        logger.critical(f"skos:prefLabel not found in {r.url}")
+        return None
+    about = about.rsplit("/", 1)[-1]
+    about = re.sub(
+        r"(?<!^)(?=[A-ZÁÉÍÓÚÜÑ])",
+        " ",
+        about
+    )
+    return about
+
+
+def str_line(s: str | None):
+    if s is None:
+        return ''
+    return re_sp.sub(" ", s).strip()
+
+
+def str_tuple(s: str | None, spl: str) -> tuple[str, ...]:
+    if s in (None, ""):
+        return tuple()
+    arr: set[str] = set()
+    for x in re.split(spl, s):
+        x = x.strip()
+        if len(x) > 0:
+            arr.add(x)
+    return tuple(sorted(arr))
+
+
+def date_to_str(d: date | datetime | None):
+    if d is None:
+        return None
+    if isinstance(d, datetime):
+        return d.strftime("%Y-%m-%d %H:%M")
+    if isinstance(d, date):
+        return d.strftime("%Y-%m-%d")
+    raise ValueError(d)
+
+
+class Place(NamedTuple):
+    latitude: float
+    longitude: float
+    location: str
+    address: str
+
+
+class Item(NamedTuple):
+    id: int
+    url: str
+    title: str
+    description: str
+    dtstart: str
+    dtend: str
+    recurrence: bool
+    audience: tuple[str, ...] = tuple()
+    typ: Optional[str] = None
+    place: Optional[Place] = None
+    price: Optional[str] = None
+
+    @staticmethod
+    def build(*args, **kwargs):
+        obj = get_obj(*args, **kwargs)
+        if obj is None:
+            return None
+        for k, v in list(obj.items()):
+            if isinstance(v, list):
+                obj[k] = tuple(v)
+            if k == "place" and isinstance(v, dict):
+                obj[k] = Place(**v)
+        return Item(**obj)
+
+
+class MadridEsDictWrapper(DictWrapper):
+
+    def get_api_org(self):
+        url = self.get_dict_or_empty('relation').get_str_or_none('@id') or ''
+        if re.match(r"^https://datos\.madrid\.es/egob/catalogo/tipo/entidadesyorganismos/\S+\.json$", url):
+            return url
+
+    @property
+    def __organismo(self):
+        org = self.get('__organization__')
+        if isinstance(org, dict):
+            return DictWrapper(org)
+        return DictWrapper({})
+
+    def is_free(self):
+        if self.get_bool('free'):
+            return True
+        if self.get_bool_or_none('__free__') is True:
+            return True
+        return False
+
+    def get_location(self):
+        loc = self.get_dict_or_none('location')
+        if loc:
+            return loc
+        loc = self.__organismo.get_dict_or_none('location')
+        if loc:
+            return loc
+
+    def event_location(self):
+        el = self.get_str_or_none('event-location')
+        if el is not None:
+            return el
+        tt = self.__organismo.get_str_or_none('title')
+        if tt is not None:
+            return tt
+        org = self.__organismo.get_dict_or_empty('organization')
+        nam = org.get_str_or_none('organization-name')
+        if nam is not None:
+            return nam
+        raise ValueError(f"[{self.get('id')}] event-location no encontrado")
+
+    def get_address(self):
+        area = self.__get_address()
+        if area:
+            return ", ".join([
+                area.get_str('street-address'),
+                area.get_str('postal-code'),
+                area.get_str('locality')
+            ])
+        raise ValueError(f"[{self.get('id')}] address no encontrado")
+
+    def __get_address(self):
+        addr = self.get_dict_or_none('address')
+        area = self.get_dict_or_none('area')
+        if area:
+            return area
+        addr = self.__organismo.get_dict_or_none("address")
+        if addr:
+            return addr
+
+
+class Dataset(NamedTuple):
+    events: list[dict]
+    organizations: dict[str, dict]
+
+    @staticmethod
+    def build(*args, **kwargs):
+        obj = get_obj(*args, **kwargs)
+        if obj is None:
+            return None
+        return Dataset(**obj)
+
+
+class ApiMadridEs:
+
+    def __obj_to_event(self, obj: dict):
+        i = MadridEsDictWrapper(obj)
+        place = None
+        dtend = i.get_datetime("dtend", "%Y-%m-%d %H:%M:%S.0")
+        loc = i.get_location()
+        place = Place(
+            latitude=loc.get_float('latitude'),
+            longitude=loc.get_float('longitude'),
+            location=i.event_location(),
+            address=i.get_address()
+        ) if loc else None
+        dtstart = i.get_datetime("dtstart", "%Y-%m-%d %H:%M:%S.0")
+        hm_tm = i.get_str_or_none('time')
+        if hm_tm not in (None, dtstart.strftime("%H:%M")):
+            logger.critical(f"time={hm_tm} dtstart={dtstart} in {obj}")
+        e = Item(
+            id=i.get_int('id'),
+            typ=i.get_str_or_none("@type"),
+            url=i.get_str('link'),
+            title=str_line(i.get_str('title')),
+            price=i.get_str_or_none('price'),
+            description=i.get_str_or_none('description'),
+            dtstart=date_to_str(dtstart),
+            dtend=date_to_str(dtend),
+            audience=str_tuple(i.get_str_or_none("audience"), r"\s*,\s*"),
+            recurrence=i.get("recurrence") is not None,
+            place=place
+        )
+        return e
+
+    @TupleCache("rec/apimadrides/dataset.json", builder=Dataset.build)
+    def __get_events(self):
+        FREE_IF_IN = (
+            'https://datos.madrid.es/egob/catalogo/206717-0-agenda-eventos-bibliotecas.json',
+        )
+        events_dict: dict[int, dict] = {}
+        sources: dict[str, list[dict]] = Getter(
+            onread=rq_to_graph_list
+        ).get(
+            "https://datos.madrid.es/egob/catalogo/300107-0-agenda-actividades-eventos.json",
+            "https://datos.madrid.es/egob/catalogo/206717-0-agenda-eventos-bibliotecas.json",
+            "https://datos.madrid.es/egob/catalogo/206974-0-agenda-eventos-culturales-100.json",
+            'https://datos.madrid.es/egob/catalogo/212504-0-agenda-actividades-deportes.json'
+        )
+        sources = dict(sorted(sources.items(), key=lambda kv: (-len(kv[1]), kv[0])))
+        for i, (url, evs) in enumerate(sources.items()):
+            logger.info(f"[{len(evs):5d}] {url}")
+            new_ids: set[int] = set()
+            for e in evs:
+                _id_ = int(e['id'])
+                if _id_ not in events_dict:
+                    new_ids.add(_id_)
+                obj = events_dict.get(_id_, {
+                    "__source__": url
+                })
+                for k, v in e.items():
+                    if obj.get(k) is None:
+                        obj[k] = v
+                if url in FREE_IF_IN:
+                    obj['__free__'] = True
+                events_dict[_id_] = obj
+            if i > 0:
+                count = len(new_ids)
+                logger.info(f"[+{count:4d}] {url} {', '.join(map(str, sorted(new_ids)))}")
+
+        events = list(events_dict.values())
+        orgs: set[str] = set()
+        for e in events:
+            relation = e.get("relation")
+            if isinstance(relation, dict):
+                url = relation.get('@id')
+                if isinstance(url, str):
+                    url = url.strip()
+                    if re_org_api.match(url):
+                        orgs.add(url)
+                        e['__organization__'] = url
+
+        org_data: dict[str, dict] = Getter(
+            onread=rq_to_graph_list_len_1
+        ).get(*orgs)
+
+        dts = Dataset(
+            events=list(events_dict.values()),
+            organizations=org_data
+        )
+        return dts
+
+    @TupleCache("rec/apimadrides/items.json", builder=Item.build)
+    def get_events(self):
+        dataset = self.__get_events()
+        size = len(dataset.events)
+        events: set[Item] = set()
+        for e in dataset.events:
+            org = dataset.organizations.get(e.get('__organization__'))
+            if isinstance(org, dict):
+                e['__organization__'] = org
+            x = self.__obj_to_event(e)
+            if x is not None:
+                events.add(x)
+        logger.info(f"[{size:5d}] Total")
+        discard = size-len(events)
+        if discard:
+            logger.info(f"[-{discard:4d}] Descartados")
+
+        typs: set[str] = set()
+        for e in events:
+            if e.typ:
+                typs.add(e.typ)
+
+        typs_label: dict[str, list[dict]] = Getter(
+            onread=rq_to_label
+        ).get(*typs)
+
+        evs: set[Item] = set()
+        for e in events:
+            evs.add(e._replace(
+                typ=typs_label.get(e.typ)
+            ))
+
+        return tuple(sorted(evs))
+
+
+if __name__ == "__main__":
+    from core.log import config_log
+    config_log("log/apimadrides.log", log_level=(logging.INFO))
+    m = ApiMadridEs()
+    print(len(m.get_events()))
---
+++ core/madrid_es/form.py
@@ -0,0 +1,329 @@
+from core.web import Web, WebException, Driver, get_text, get_query, buildSoup
+from urllib.parse import urljoin
+from bs4 import Tag, BeautifulSoup
+import re
+from core.util import plain_text, get_domain, trim
+import logging
+from functools import cached_property, cache
+from types import MappingProxyType
+from typing import NamedTuple
+from core.fetcher import Getter, rq_to_text
+from core.util import normalize_url, get_obj
+from urllib.parse import urlencode
+from typing import Optional, Iterable
+from aiohttp import ClientResponse
+from core.cache import TupleCache
+
+
+logger = logging.getLogger(__name__)
+re_sp = re.compile(r"\s+")
+
+
+def get_vgnextoid(url: str | Tag):
+    if isinstance(url, Tag):
+        url = url.attrs.get("href")
+    if url is None:
+        return None
+    if not isinstance(url, str):
+        raise ValueError(url)
+    url = url.strip()
+    if len(url) == 0 or get_domain(url) != "madrid.es":
+        return None
+    qr = get_query(url)
+    id = qr.get("vgnextoid")
+    if not isinstance(id, str):
+        return None
+    id = id.strip()
+    if len(id) == 0:
+        return None
+    return id
+
+
+class Place(NamedTuple):
+    latitude: float
+    longitude: float
+    location: str
+    address: str
+
+
+class Item(NamedTuple):
+    url: str
+    title: str
+    place: Optional[Place] = None
+    audience: tuple[str, ...] = tuple()
+    typ: tuple[str, ...] = tuple()
+    free: bool = False
+
+    @staticmethod
+    def build(*args, **kwargs):
+        obj = get_obj(*args, **kwargs)
+        if obj is None:
+            return None
+        for k, v in list(obj.items()):
+            if isinstance(v, list):
+                obj[k] = tuple(v)
+            if k == "place" and isinstance(v, dict):
+                obj[k] = Place(**v)
+        return Item(**obj)
+
+
+class Index(NamedTuple):
+    total: int
+    urls: tuple[str, ...]
+    items: tuple[Item, ...] = tuple()
+
+
+def soup_to_items(soup: BeautifulSoup):
+    items: set[Item] = set()
+    for div in soup.select("#listSearchResults ul.events-results li div.event-info"):
+        a = div.select_one("a.event-link")
+        vgnextoid = get_vgnextoid(a)
+        if vgnextoid is None:
+            continue
+        lc = div.select_one("a.event-location")
+        pl = Place(
+            latitude=float(lc.attrs["data-latitude"]),
+            longitude=float(lc.attrs["data-longitude"]),
+            location=trim(lc.attrs["title"]),
+            address=trim(lc.attrs["data-direction"]),
+        ) if lc else None
+        it = Item(
+            url=a.attrs['href'],
+            title=get_text(a),
+            place=pl
+        )
+        items.add(it)
+    return tuple(items)
+
+
+async def rq_to_items(r: ClientResponse):
+    soup = buildSoup(str(r.url), await r.read())
+    items = soup_to_items(soup)
+    return items
+
+
+async def rq_to_index(r: ClientResponse):
+    soup = buildSoup(str(r.url), await r.read())
+    total = int(get_text(soup.select_one(".results-total strong")) or "0")
+    if total == 0:
+        return Index(
+            total=total,
+            urls=tuple(),
+        )
+    slc_size = ".results-displayed strong"
+    text = get_text(soup.select_one(".results-displayed strong"))
+    if text is None:
+        raise WebException(f"{slc_size} NOT FOUND in {r.url}")
+    m = re.match(r"^\s*1\s*-\s*(\d+)\s*$", text)
+    if m is None:
+        raise WebException(f"{slc_size} INVALID FORMAT '{text}' in {r.url}")
+    page_size = int(m.group(1))
+    if page_size > total:
+        raise WebException(f"{slc_size} TOO BIG {page_size} > {total} in {r.url}")
+    items = soup_to_items(soup)
+    if page_size == total:
+        return Index(
+            total=total,
+            items=items,
+            urls=tuple(),
+        )
+    slc_a_next = "li.next a.pagination-text"
+    a_next = soup.select_one(slc_a_next)
+    if a_next is None:
+        raise WebException(f"{slc_a_next} NOT FOUND in {r.url}")
+    href = a_next.attrs["href"]
+    onclick = a_next.attrs.get("onclick")
+    if isinstance(onclick, str):
+        m = re.match(
+            r".*ajaxDivChange\(['\"]([^'\"]+).*",
+            onclick
+        )
+        if m:
+            href = urljoin(str(r.url), m.group(1))
+    pages = ((total - 1) // page_size + 1)
+    href = normalize_url(href, "page")
+    urls = [href]
+    while len(urls) < pages:
+        last_url = urls[-1]
+        next_page = int(re.search(r"page=(\d+)", last_url).group(1)) + 1
+        next_url = re.sub(r"page=\d+", f"page={next_page}", last_url)
+        urls.append(next_url)
+    return Index(
+        total=total,
+        items=items,
+        urls=tuple(urls),
+    )
+
+
+class FormSearch:
+    AGENDA = "https://www.madrid.es/portales/munimadrid/es/Inicio/Actualidad/Actividades-y-eventos/?vgnextfmt=default&vgnextchannel=ca9671ee4a9eb410VgnVCM100000171f5a0aRCRD"
+    TAXONOMIA = "https://www.madrid.es/ContentPublisher/jsp/apl/includes/XMLAutocompletarTaxonomias.jsp?taxonomy=/contenido/actividades&idioma=es&onlyFirstLevel=true"
+
+    def __init__(self):
+        self.__w = Web()
+        self.__w.s = Driver.to_session(
+            "firefox",
+            "https://www.madrid.es",
+            session=self.__w.s,
+        )
+        self.__getter_index = Getter(
+            onread=rq_to_index,
+            headers=self.__w.s.headers,
+            cookie_jar=self.__w.s.cookies
+        )
+        self.__getter_items = Getter(
+            onread=rq_to_items,
+            headers=self.__w.s.headers,
+            cookie_jar=self.__w.s.cookies
+        )
+        self.usuarios = MappingProxyType(self.__get_options("#usuario"))
+        self.tipos = MappingProxyType(self.__get_tipos())
+        self.__loaded = False
+        self.__all: tuple[Item, ...] = tuple()
+        self.__free: tuple[str, ...] = tuple()
+        self.__user: MappingProxyType[str, tuple[str, ...]] = {}
+        self.__typ: MappingProxyType[str, tuple[str, ...]] = {}
+
+    def __load(self):
+        if self.__loaded:
+            return
+        logger.info("INI: FormSearch.preload()")
+        ALL = self.build_search_url()
+        FREE = self.build_search_url(gratuita="1")
+        USER = {}
+        TYP = {}
+        for k in self.usuarios.keys():
+            USER[k] = self.build_search_url(usuario=k)
+        for k in self.tipos.keys():
+            TYP[k] = self.build_search_url(tipo=k)
+
+        urls: set[str] = set({
+            ALL,
+            FREE,
+            *USER.values(),
+            *TYP.values()
+        })
+        url_index: dict[str, Index] = self.__getter_index.get(*urls)
+        pages: set[str] = set()
+        for i in url_index.values():
+            pages.update(i.urls)
+        url_items: dict[str, tuple[Item, ...]] = self.__getter_items.get(*pages)
+
+        def _get_items(u: str):
+            index = url_index[u]
+            items = set(index.items)
+            for x in index.urls:
+                items.update(url_items[x])
+            return tuple(items)
+
+        user_items: dict[str, tuple[Item, ...]] = {}
+        typ_items: dict[str, tuple[Item, ...]] = {}
+        all_items = set(_get_items(ALL))
+        free_items = _get_items(FREE)
+        all_items.update(free_items)
+        for k, u in USER.items():
+            user_items[k] = _get_items(u)
+            all_items.update(user_items[k])
+        for k, u in TYP.items():
+            typ_items[k] = _get_items(u)
+            all_items.update(free_items)
+
+        def _to_ids(arr: Iterable[Item]):
+            return tuple(get_vgnextoid(i.url) for i in arr)
+
+        self.__all = tuple(all_items)
+        self.__free = _to_ids(free_items)
+        self.__user = MappingProxyType({
+            k: _to_ids(items)
+            for k, items in
+            user_items.items()
+        })
+        self.__typ = MappingProxyType({
+            k: _to_ids(items)
+            for k, items in
+            typ_items.items()
+        })
+        logger.info("FIN: FormSearch.preload()")
+        self.__loaded = True
+
+    def get(self, url, *args, **kwargs) -> BeautifulSoup:
+        if self.__w.url != url:
+            logger.debug(url)
+            self.__w.get(url, *args, **kwargs)
+        title = get_text(self.__w.soup.select_one("title"))
+        if title == "Access Denied":
+            body = get_text(self.__w.soup.select_one("body"))
+            body = re.sub(r"^Access Denied\s+", "", body or "")
+            raise ValueError(f"{url} {title} {body}".strip())
+        return self.__w.soup
+
+    @cache
+    def __prepare_search(self):
+        self.get(FormSearch.AGENDA)
+        action, data = self.__w.prepare_submit("#generico1", enviar="buscar")
+        if action is None:
+            raise WebException(f"#generico1 NOT FOUND in {self.__w.url}")
+        for k in ("gratuita", "movilidad"):
+            if k in data:
+                del data[k]
+        data["tipo"] = "-1"
+        data["distrito"] = "-1"
+        data["usuario"] = "-1"
+        return action, MappingProxyType(data)
+
+    def build_search_url(self, **kwargs):
+        action, action_data = self.__prepare_search()
+        for k, v in action_data.items():
+            if k not in kwargs:
+                kwargs[k] = v
+        url = action + '?' + urlencode(kwargs)
+        return url
+
+    def __get_options(self, slc: str):
+        data: dict[str, str] = {}
+        soup = self.get(FormSearch.AGENDA)
+        for o in soup.select(slc+" option"):
+            k = o.attrs["value"]
+            v = re_sp.sub(" ", o.get_text()).strip()
+            if k != "-1":
+                data[k] = v
+        return data
+
+    def __get_tipos(self):
+        data: dict[str, str] = {}
+        soup = self.get(FormSearch.TAXONOMIA, parser="xml")
+        for n in soup.find_all('item'):
+            value = n.find('value').string.strip()
+            text = re_sp.sub(" ", n.find('text').string).strip()
+            data[value] = text
+        return data
+
+    @TupleCache("rec/apimadrides/form.json", builder=Item.build)
+    def get_events(self):
+        self.__load()
+        items: set[Item] = set()
+        for i in self.__all:
+            vid = get_vgnextoid(i.url)
+            aud: set[str] = set()
+            typ: set[str] = set()
+            for k, v in self.usuarios.items():
+                if vid in self.__user[k]:
+                    aud.add(v)
+            for k, v in self.tipos.items():
+                if vid in self.__typ[k]:
+                    typ.add(v)
+            i = i._replace(
+                free=vid in self.__free,
+                audience=tuple(sorted(aud)),
+                typ=tuple(sorted(typ))
+            )
+            items.add(i)
+        return tuple(sorted(items))
+
+
+if __name__ == "__main__":
+    from core.log import config_log
+    config_log("log/search.log")
+    FS = FormSearch()
+    evs = FS.get_events()
+    print(len(evs))
