15e0ddc5c40ba43691c429c146a597dfde4aefad
s-nt-s <santos82h@gmail.com>
2026-03-13 17:44:35 +0100

    dwn

--- dwn.sh
+++ dwn.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+URL="https://s-nt-s.github.io/imdb-sql/imdb.sqlite.zst"
 rm -rf imdb.sqlite
-curl -L https://s-nt-s.github.io/imdb-sql/imdb.sqlite.zst | zstd -d -o imdb.sqlite
+echo "[..] $URL"
+curl -sqL "$URL" | zstd -dq -o imdb.sqlite
+if [ $? -eq 0 ]; then
+    echo "[OK] $URL"
+else
+    echo "[KO] $URL"
+fi
 ls -lah imdb.sqlite
