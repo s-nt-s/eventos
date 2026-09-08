d0dce4746f69691676e4c8d387f45ff96379f18a
s-nt-s <santos82h@gmail.com>
2026-09-08 11:43:09 +0200

    Update ics.py

--- core/ics.py
+++ core/ics.py
@@ -198,7 +198,7 @@ class IcsEventWrapper:
     def __find_hours(self):
         txt = self.__get_text("SUMMARY") or ''
         hms: set[tuple[int, int]] = set()
-        for h, m in re.findall(r"\b([01]\d|2[0-4]):(\d[0-5]\d)\b", txt):
+        for h, m in re.findall(r"\b([01]\d|2[0-4]):(\d[0-5]\d)\s*h", txt):
             hms.add((int(h), int(m)))
         return tuple(sorted(hms))
     
